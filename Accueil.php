fc87bc86753dc62fed2e6410511278c9852979a8
MylanSe <92014727+MylanSe@users.noreply.github.com>
2022-10-11 11:41:49 +0200

    Menu burger
    azerty

--- Accueil.php
+++ Accueil.php
@@ -12,5 +12,24 @@
   </head>
 
 <body>
+<div id="mySidenav" class="sidenav">
+  <a id="closeBtn" href="#" class="close">×</a>
+  <ul>
+    <li><a href="#">A propos</a></li>
+    <li><a href="#">Nos services</a></li>
+    <li><a href="#">Témoignages</a></li>
+    <li><a href="#">Contact</a></li>
+  </ul>
+</div>
+
+<a href="#" id="openBtn">
+  <span class="burger-icon">
+    <span></span>
+    <span></span>
+    <span></span>
+  </span>
+</a>
+
 </body>
+<script src="menuburger.js"></script>
 </html>
