2b0b3bfecb3b8ea2ded00521f7b2598120c46dc3
Mathiasprg07 <parageau.mathias@gmail.com>
2022-11-08 08:36:51 +0100

    creation animaux et plan

---
+++ Plan.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+ <head>
+ <?php include("include.php")?>
+      <!-- Bootstrap CSS -->
+      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+      <link rel="stylesheet" href="style.css">
+   <title>Zoo de Bovalle</title>
+ </head>
+ <body>
+  
+
+<form action="https://buy.stripe.com/test_9AQg1n4bH2CEeGI8ww">
+ <button class="btn btn-billetterie">Plan du Zoo</button>
+</form>
+
+
+ </body>
+ <footer>
+ <?php include("footer.php")?>
+</footer>
+</html>
