65df63070113ee4f4038e9c6e5dc458ecaa6140c
maxence0724 <maxence.dufaud24@hotmail.com>
2022-11-08 08:13:43 +0100

    footer avec css
    footer css

---
+++ Footer.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html>
+ <head>
+   <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+   <link rel="shortcut icon" type="image/ico" href="img/favicon.gif" />
+   <link rel="stylesheet" type="text/css" href="style.css" />
+   <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+    <!-- Bootstrap CSS -->
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+<link rel="stylesheet" href="style.css">
+    <title>Zoo de Bovalle</title>
+ </head>
+ <body>
+ <footer>
+    <div class="container">
+        <div>
+            <h2 class="text-center">Nous contacter</h2>
+        </div>    
+
+        <div class="row">
+            <div class="col-lg-3 col-md-3 col-sm-4 col-xs-12">
+                <ul class="reseaux">
+                <a href="#"><img src="logos/facebook.png"></a>
+                </ul>
+            </div>
+            
+            <div class="col-lg-3 col-md-3 col-sm-4 col-xs-12">
+                <ul class="reseaux">
+                <a href="#"><img src="logos/youtube.png"></a>
+                </ul>
+            </div>
+       
+            <div class="col-lg-3 col-md-3 col-sm-4 col-xs-12">
+                <ul class="reseaux">
+                <a href="#"><img src="logos/twitter.png"></a>
+                </ul>
+            </div>
+            <div class="col-lg-3 col-md-3 col-sm-4 col-xs-12">
+                <ul class="reseaux">
+                    
+                <a href="#"><img src="logos/instagram.png"></a>
+                    
+                </ul>
+            </div>
+       </div> 
+    </div>
+</footer>
+ </body>
+
+</html>
