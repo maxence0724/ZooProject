e536389b7b471566c6c0262b4a66146e835fffa2
Mathiasprg07 <parageau.mathias@gmail.com>
2022-11-08 08:18:13 +0100

    08/11
    modif style et bouton billetterie

--- Accueil.php
+++ Accueil.php
@@ -4,7 +4,7 @@
  <?php include("include.php")?>
       <!-- Bootstrap CSS -->
       <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-      <link rel="stylesheet" href="style.css">  
+      <link rel="stylesheet" href="style.css">
    <title>Zoo de Bovalle</title>
  </head>
  <body>
