c9091fe37047203c883caa1d61aa9669a70dce51
maxence0724 <maxence.dufaud24@hotmail.com>
2022-11-15 11:42:17 +0100

    Create troll_calendar.php

---
+++ troll_calendar.php
@@ -0,0 +1,192 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+	
+	<meta http-equiv="content-type" content="text/html; charset=windows-1252"/>
+	<title></title>
+	<meta name="generator" content="LibreOffice 7.3.2.2 (Windows)"/>
+	<meta name="created" content="2022-10-04T08:10:08.501000000"/>
+	<meta name="changed" content="2022-10-04T09:14:28.133000000"/>
+	
+	<style type="text/css">
+		body,div,table,thead,tbody,tfoot,tr,th,td,p { font-family:"Liberation Sans"; font-size:x-small }
+		a.comment-indicator:hover + comment { background:#ffd; position:absolute; display:block; border:1px solid black; padding:0.5em;  } 
+		a.comment-indicator { background:red; display:inline-block; border:1px solid black; width:0.5em; height:0.5em;  } 
+		comment { display:none;  } 
+	</style>
+	
+</head>
+
+<body>
+<table cellspacing="0" border="0">
+	<colgroup width="128"></colgroup>
+	<colgroup width="245"></colgroup>
+	<colgroup width="294"></colgroup>
+	<colgroup width="254"></colgroup>
+	<colgroup span="4" width="249"></colgroup>
+	<tr>
+		<td height="26" align="center"><br></td>
+		<td align="center">LUNDI</td>
+		<td align="center">MARDI</td>
+		<td align="center">MERCREDI</td>
+		<td align="center">JEUDI</td>
+		<td align="center">VENDREDI</td>
+		<td align="center">SAMEDI</td>
+		<td align="center">DIMANCHE</td>
+	</tr>
+	<tr>
+		<td height="26" align="center">8h00</td>
+		<td align="left" bgcolor="#E8F2A1">ouverture du parc</td>
+		<td align="left" bgcolor="#E8F2A1">ouverture du parc </td>
+		<td align="left" bgcolor="#E8F2A1">ouverture du parc</td>
+		<td align="left" bgcolor="#E8F2A1">ouverture du parc</td>
+		<td align="left" bgcolor="#E8F2A1">ouverture du parc</td>
+		<td align="left" bgcolor="#E8F2A1">ouverture du parc</td>
+		<td align="left" bgcolor="#E8F2A1">ouverture du parc</td>
+	</tr>
+	<tr>
+		<td height="26" align="center">9h00</td>
+		<td align="left"><br></td>
+		<td align="left"><br></td>
+		<td align="left"><br></td>
+		<td align="left"><br></td>
+		<td align="left"><br></td>
+		<td align="left"><br></td>
+		<td align="left"><br></td>
+	</tr>
+	<tr>
+		<td height="26" align="center">10h00</td>
+		<td align="left" bgcolor="#B4C7DC">Spectacle des dauphins</td>
+		<td align="left" bgcolor="#B4C7DC">Spectacle des dauphins</td>
+		<td align="left" bgcolor="#B4C7DC">Spectacle des dauphins</td>
+		<td align="left" bgcolor="#B4C7DC">Spectacle des dauphins</td>
+		<td align="left" bgcolor="#B4C7DC">Spectacle des dauphins</td>
+		<td align="left" bgcolor="#B4C7DC">Spectacle des dauphins</td>
+		<td align="left" bgcolor="#B4C7DC">Spectacle des dauphins</td>
+	</tr>
+	<tr>
+		<td height="26" align="center">11h00</td>
+		<td align="left"><br></td>
+		<td align="left"><br></td>
+		<td align="left"><br></td>
+		<td align="left"><br></td>
+		<td align="left"><br></td>
+		<td align="left"><br></td>
+		<td align="left"><br></td>
+	</tr>
+	<tr>
+		<td height="26" align="center">12h00</td>
+		<td align="left"><br></td>
+		<td align="left"><br></td>
+		<td align="left"><br></td>
+		<td align="left"><br></td>
+		<td align="left"><br></td>
+		<td align="left"><br></td>
+		<td align="left"><br></td>
+	</tr>
+	<tr>
+		<td height="26" align="center">13h00</td>
+		<td align="left" bgcolor="#FFAA95">Nourrissage des lions</td>
+		<td align="left" bgcolor="#FFAA95">Nourrissage des lions</td>
+		<td align="left" bgcolor="#FFAA95">Nourrissage des lions </td>
+		<td align="left" bgcolor="#FFAA95">Nourrissage des lions</td>
+		<td align="left" bgcolor="#FFAA95">Nourrissage des lions </td>
+		<td align="left" bgcolor="#FFAA95">Nourrissage des lions </td>
+		<td align="left" bgcolor="#FFAA95">Nourrissage des lions </td>
+	</tr>
+	<tr>
+		<td height="26" align="center">14h00</td>
+		<td align="left"><br></td>
+		<td align="left"><br></td>
+		<td align="left"><br></td>
+		<td align="left"><br></td>
+		<td align="left"><br></td>
+		<td align="left"><br></td>
+		<td align="left"><br></td>
+	</tr>
+	<tr>
+		<td height="26" align="center">15h00</td>
+		<td align="left" bgcolor="#AFD095">Nourrissage des &eacute;l&eacute;phants</td>
+		<td align="left" bgcolor="#AFD095">Nourrissage des &eacute;l&eacute;phants</td>
+		<td align="left" bgcolor="#AFD095">Nourrissage des &eacute;l&eacute;phants</td>
+		<td align="left" bgcolor="#AFD095">Nourrissage des &eacute;l&eacute;phants</td>
+		<td align="left" bgcolor="#AFD095">Nourrissage des &eacute;l&eacute;phants</td>
+		<td align="left" bgcolor="#AFD095">Nourrissage des &eacute;l&eacute;phants</td>
+		<td align="left" bgcolor="#AFD095">Nourrissage des &eacute;l&eacute;phants</td>
+	</tr>
+	<tr>
+		<td height="26" align="center">16h00</td>
+		<td align="left"><br></td>
+		<td align="left"><br></td>
+		<td align="left"><br></td>
+		<td align="left"><br></td>
+		<td align="left"><br></td>
+		<td align="left"><br></td>
+		<td align="left"><br></td>
+	</tr>
+	<tr>
+		<td height="26" align="center">17h00</td>
+		<td align="left" bgcolor="#E0C2CD">Spectacle des oiseaux </td>
+		<td align="left" bgcolor="#E0C2CD">Spectacle des oiseaux </td>
+		<td align="left" bgcolor="#E0C2CD">Spectacle des oiseaux </td>
+		<td align="left" bgcolor="#E0C2CD">Spectacle des oiseaux</td>
+		<td align="left" bgcolor="#E0C2CD">Spectacle des oiseaux</td>
+		<td align="left" bgcolor="#E0C2CD">Spectacle des oiseaux </td>
+		<td align="left" bgcolor="#E0C2CD">Spectacle des oiseaux</td>
+	</tr>
+	<tr>
+		<td height="26" align="center">18h00</td>
+		<td align="left"><br></td>
+		<td align="left"><br></td>
+		<td align="left"><br></td>
+		<td align="left"><br></td>
+		<td align="left"><br></td>
+		<td align="left"><br></td>
+		<td align="left"><br></td>
+	</tr>
+	<tr>
+		<td height="26" align="center">19h00</td>
+		<td align="left"><br></td>
+		<td align="left"><br></td>
+		<td align="left"><br></td>
+		<td align="left"><br></td>
+		<td align="left"><br></td>
+		<td align="left"><br></td>
+		<td align="left"><br></td>
+	</tr>
+	<tr>
+		<td height="26" align="center">20h00</td>
+		<td align="left"><br></td>
+		<td align="left"><br></td>
+		<td align="left"><br></td>
+		<td align="left"><br></td>
+		<td align="left"><br></td>
+		<td align="left"><br></td>
+		<td align="left"><br></td>
+	</tr>
+	<tr>
+		<td height="26" align="center">21h00</td>
+		<td align="left" bgcolor="#FFDBB6">Spectacle de lumi&egrave;res</td>
+		<td align="left" bgcolor="#FFDBB6">Spectacle de lumi&egrave;res</td>
+		<td align="left" bgcolor="#FFDBB6">Spectacle de lumi&egrave;res</td>
+		<td align="left" bgcolor="#FFDBB6">Spectacle de lumi&egrave;res </td>
+		<td align="left" bgcolor="#FFDBB6">Spectacle de lumi&egrave;re </td>
+		<td align="left" bgcolor="#FFDBB6">Spectacle de lumi&egrave;re</td>
+		<td align="left" bgcolor="#FFDBB6">Spectacle de lumi&egrave;re </td>
+	</tr>
+	<tr>
+		<td height="26" align="center">22h00 </td>
+		<td align="left"><br></td>
+		<td align="left"><br></td>
+		<td align="left"><br></td>
+		<td align="left"><br></td>
+		<td align="left"><br></td>
+		<td align="left"><br></td>
+		<td align="left"><br></td>
+	</tr>
+</table>
+<!-- ************************************************************************** -->
+</body>
+
+</html>
