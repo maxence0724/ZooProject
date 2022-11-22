a8b93db07df1576a1913b11bae8a40ee4cb9ff4d
MylanSe <92014727+MylanSe@users.noreply.github.com>
2022-11-22 09:50:24 +0100

    Add files via upload

--- Description_Animaux.php
+++ Description_Animaux.php
@@ -1,7 +1,8 @@
 <!DOCTYPE html>
 <html>
  <head>
- <?php include("include.php")?>
+ <?php include("include.php");
+    include("connexion.php")?>
       <!-- Bootstrap CSS -->
       <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
       <link rel="stylesheet" href="style.css">
@@ -11,66 +12,27 @@
 <div class=BoxGlobal>
 <br>
 
-<div class=aigle>
-  <img src="img/aigle.png" alt="" class="description_img">
-  <h3>Pyguargue à tête blanche<h3>
-</div>
-<div class=perroquet_bleu>
-  <img src="img/perroquet_bleu.png" alt="" class="description_img">
-  <h3>Ara bleu et jaune<h3>
-</div>
-<div class=perroquet>
-  <img src="img/perroquet.png" alt="" class="description_img">
-  <h3>Ara chloroptère<h3>
-</div>
-<div class=autruche>
-  <img src="img/autruche.png" alt="" class="description_img">
-  <h3>Autruche d'Afrique<h3>
-</div>
-<div class=singe>
-  <img src="img/singe.png" alt="" class="description_img">
-  <h3>Chimpanzé<h3>
-</div>
-<div class=dragon>
-  <img src="img/dragon.png" alt="" class="description_img">
-  <h3>Dragon de komodo<h3>
-</div>
-<div class=elephant>
-  <img src="img/elephant.png" alt="" class="description_img">
-  <h3>Eléphant d'Afrique<h3>
-</div>
-<div class=girafe>
-  <img src="img/girafe.png" alt="" class="description_img">
-  <h3>Girafe<h3>
-</div>
-<div class=gorille>
-  <img src="img/gorille.png" alt="" class="description_img">
-  <h3>Gorille des plaines<h3>
-</div>
-<div class=guepard>
-  <img src="img/guepard.png" alt="" class="description_img">
-  <h3>Guépard<h3>
-</div>
-<div class=hipopo>
-  <img src="img/hipopo.png" alt="" class="description_img">
-  <h3>Hippopotame amphibie<h3>
-</div>
-<div class=hyene>
-  <img src="img/hyene.png" alt="" class="description_img">
-  <h3>Hyène tachetée<h3>
-</div>
-<div class=leopard>
-  <img src="img/leopard.png" alt="" class="description_img">
-  <h3>Jaguar<h3>
-</div>
-<div class=kangourou>
-  <img src="img/kangourou.png" alt="" class="description_img">
-  <h3>Kangourou roux<h3>
-</div>
-<div class=koala>
-  <img src="img/koala.png" alt="" class="description_img">
-  <h3>Koala<h3>
-</div>
+<?php 
+
+$requete = $zoo->prepare('SELECT espèce, idAnimal FROM animal');
+$requete->execute();
+$compteur = 0 ;
+while($animal = $requete->fetch()){
+    $compteur++;
+    if($compteur%4 == 1) echo '<div class=aigle><h3>';
+    if($compteur%4 == 2) echo '<div class=perroquet_bleu><h3>';
+    if($compteur%4 == 3) echo '<div class=perroquet><h3>';
+    if($compteur%4 == 0) echo '<div class=autruche><h3>';  
+    echo $animal['espèce'];
+    echo '<h3></div>';
+}
+
+?>
+
+
+
+
+
 
 <div class=square>
 <a href="insertion.php">
